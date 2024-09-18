cddf58d69dc1fd77b913a902e3da1323167686d4
Muhammad Rehan <muhammadrehan125768@gmail.com>
2024-09-19 01:15:08 +0500

    Add verify email route

---
+++ src/app/api/users/verifyemail/route.ts
@@ -0,0 +1,51 @@
+import { NextRequest, NextResponse } from "next/server";
+import User from "@/models/userModel";
+import bcryptjs from "bcryptjs";
+import sendEmail from "@/helpers/mailer";
+import { connectDB } from "@/db/db";
+
+connectDB();
+
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.json();
+
+    const { token } = reqBody;
+
+    const user = await User.findOne({
+      verifyToken: token,
+      verifyTokenExpiry: {
+        $gt: Date.now(),
+      },
+    });
+
+    if (!user) {
+      return NextResponse.json(
+        {
+          error: "Invalid Token",
+        },
+        { status: 400 }
+      );
+    }
+
+    user.isVerified = true;
+    user.verifyToken = undefined;
+    user.verifyTokenExpiry = undefined;
+
+    await user.save();
+    return NextResponse.json(
+      {
+        message: "Email Verify Successfully",
+        success: true,
+      },
+      { status: 200 }
+    );
+  } catch (error: any) {
+    return NextResponse.json(
+      {
+        error: error.message,
+      },
+      { status: 500 }
+    );
+  }
+}
